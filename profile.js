a1f43ab0e54b01377b6a771487200081dcc9ce4b
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-12-11 13:04:37 -0800

    more pfp changes

--- profile.js
+++ profile.js
@@ -3,10 +3,21 @@ const urlParams = new URLSearchParams(queryString);
 
 
 function showPosts(posts){
+    let i = 0;
+
     posts.forEach((post) => {
         if(post.name == urlParams.get('name')){
             post.createPost()
+            loadPfp(posts[i]) //loads pfp multiple times not great but whatever
         }
+        i++;
         
     });
 }
+
+function loadPfp(post){
+    // console.log(post)
+    let pfp = document.getElementById("pfp");
+    pfp.src = post.pfp;
+}
+
